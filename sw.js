c0173f432b4e64b753195cf91bbafa0720746ffd
Niallza <nzxtastro@proton.me>
2026-04-29 15:12:45 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-self.addEventListener('fetch', () => {});
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
